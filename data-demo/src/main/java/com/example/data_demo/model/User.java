eda14e41520c4012fcb4745e1b99da3b31b3302d
Silea-Horia <horia.silea@gmail.com>
2025-07-17 12:12:09 +0300

    mapped User class to users table

--- data-demo/src/main/java/com/example/data_demo/model/User.java
+++ data-demo/src/main/java/com/example/data_demo/model/User.java
@@ -1,5 +1,9 @@
 package com.example.data_demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
@@ -8,11 +12,25 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "firstname")
     private String firstname;
+
+    @Column(name = "lastname")
     private String lastname;
 }
