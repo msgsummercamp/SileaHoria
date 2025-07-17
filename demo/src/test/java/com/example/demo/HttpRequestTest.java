fbe834f728d15175929b6c9ee67aba29c9db428f
Silea-Horia <horia.silea@gmail.com>
2025-07-17 10:21:50 +0300

    created http request test

---
+++ demo/src/test/java/com/example/demo/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void usersShouldReturnAllUsers() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/users", String.class))
+                .contains("Alice", "Bob", "Charlie");
+    }
+}
