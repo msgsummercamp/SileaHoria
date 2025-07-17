639f692e11a81f24154aeae52de0181ef487fd4b
Silea-Horia <horia.silea@gmail.com>
2025-07-17 09:45:11 +0300

    created a service interface and an implementation

---
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.IRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements IService {
+    private final IRepository repository;
+
+    public UserService(@Autowired IRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return repository.getUsers();
+    }
+}
