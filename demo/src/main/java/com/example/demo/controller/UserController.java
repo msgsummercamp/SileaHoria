28d71808443e21719a5b8c0c6e27845f84a755ad
Silea-Horia <horia.silea@gmail.com>
2025-07-17 10:11:07 +0300

    added logging

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.IService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import java.util.List;
 @RestController
 public class UserController {
     private final IService service;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(@Autowired IService service) {
         this.service = service;
@@ -18,6 +21,7 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> users() {
+        logger.info("received request for users");
         return service.getUsers();
     }
 }
