0d9d3feaac8e7030bc4ffae6b6b75c4ad8beb293
Silea Horia <146817894+Silea-Horia@users.noreply.github.com>
2025-07-21 10:46:19 +0300

    Rest api exercises (#6)
    * initialize project
    
    * created user model
    
    * created user repo
    
    * created user services
    
    * created rest api endpoints
    
    * added response entities to every endpoint and returned appropiate status codes
    
    * implemented pagination
    
    * added validation
    
    * added custom exceptions and handling
    
    * added postman tests to resources folder
    
    * added documentation for the endpoints
    
    * addressed issues

---
+++ rest-api/src/main/java/com/example/rest_api/service/UserService.java
@@ -0,0 +1,58 @@
+package com.example.rest_api.service;
+
+import com.example.rest_api.exception.NotFoundException;
+import com.example.rest_api.model.User;
+import com.example.rest_api.repository.IUserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+    private static final int PAGE_SIZE = 4;
+
+    private final IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(String username, String email, String password, String firstname, String lastname) {
+        User newUser = new User();
+        newUser.setUsername(username);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        newUser.setFirstName(firstname);
+        newUser.setLastName(lastname);
+        return userRepository.save(newUser);
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+    }
+
+    public Page<User> findAll(int pageNumber) {
+        return userRepository.findAll(PageRequest.of(pageNumber, PAGE_SIZE));
+    }
+
+    public User updateUser(Long id, String username, String email, String password, String firstname, String lastname) {
+        User existingUser = userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User not found with id: " + id));
+
+        existingUser.setUsername(username);
+        existingUser.setEmail(email);
+        existingUser.setPassword(password);
+        existingUser.setFirstName(firstname);
+        existingUser.setLastName(lastname);
+
+        return userRepository.save(existingUser);
+    }
+
+    public void deleteUser(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new NotFoundException("User not found with id: " + id);
+        }
+        userRepository.deleteById(id);
+    }
+}
