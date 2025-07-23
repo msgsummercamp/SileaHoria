0449f828361398d8e60af08f8907dbeafa97a148
Silea-Horia <horia.silea@gmail.com>
2025-07-23 11:28:04 +0300

    done exercise

--- frontend/ng-app/src/app/app.component.ts
+++ frontend/ng-app/src/app/app.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal, WritableSignal} from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar} from "@angular/material/toolbar";
-import {NgOptimizedImage} from "@angular/common";
+import {NgIf, NgOptimizedImage} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+
+type DogResponse = {
+  status: string;
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButton, MatToolbar, NgOptimizedImage],
+  imports: [MatButton, MatToolbar, NgOptimizedImage, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-app';
+  title: string = 'ng-app';
+
+  data: WritableSignal<string> = signal('');
+  loading: WritableSignal<boolean> = signal(false);
+  error: WritableSignal<string> = signal('');
+  httpClient: HttpClient = inject(HttpClient);
+
+  loadRandomDogImage(): void {
+    this.loading.set(true);
+    this.error.set('');
+    this.httpClient
+      .get<DogResponse>("https://dog.ceo/api/breeds/image/random")
+      .subscribe({
+        next: (response: DogResponse) => {
+          if (response.status === 'success') {
+            this.data.set(response.message);
+            this.error.set('');
+          } else {
+            this.data.set('');
+            this.error.set('Failed to load dog image.');
+          }
+          this.loading.set(false);
+        },
+        error: () => {
+          this.data.set('');
+          this.error.set('Error loading image...');
+          this.loading.set(false);
+        }
+      });
+  }
 }
