4035ef456212dbf489aa8bbefbc50bbdd8fc9289
Silea-Horia <horia.silea@gmail.com>
2025-07-24 11:44:13 +0300

    login page now lazy loads

--- frontend/ng-app/src/app/app.routes.ts
+++ frontend/ng-app/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
   { path: '**', component: NotFoundComponent },
 ];
