fc603391b53f8233d5270f65ad1ca2e1e30c968b
Silea Horia <146817894+Silea-Horia@users.noreply.github.com>
2025-07-24 11:06:53 +0300

    Angular Components & Directives (#15)
    * generated login component
    
    * generated notfound component
    
    * added some html to notfound component
    
    * generated auth directive
    
    * got some logic for auth
    
    * cleaned up a bit, used classes
    
    * addressed comments
    
    * made prettier run
    
    * added project files
    
    * Delete frontend/ng-app/src/app/login/login.component.scss
    
    * Delete frontend/ng-app/src/app/not-found/not-found.component.scss

--- frontend/ng-app/src/app/app.component.ts
+++ frontend/ng-app/src/app/app.component.ts
@@ -1,36 +1,49 @@
-import {Component, inject, signal, WritableSignal} from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-import { MatToolbar} from "@angular/material/toolbar";
-import {NgIf, NgOptimizedImage} from "@angular/common";
-import {HttpClient} from "@angular/common/http";
+import { MatToolbar } from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthDirective } from './auth.directive';
 
 type DogResponse = {
   status: string;
   message: string;
-}
+};
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButton, MatToolbar, NgOptimizedImage],
+  imports: [
+    MatButton,
+    MatToolbar,
+    NgOptimizedImage,
+    NotFoundComponent,
+    AuthDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   private readonly httpClient: HttpClient = inject(HttpClient);
 
-  public readonly data: WritableSignal<string> = signal('');
-  public readonly loading: WritableSignal<boolean> = signal(false);
-  public readonly error: WritableSignal<string> = signal('');
+  public readonly data = signal<string>('');
+  public readonly loading = signal<boolean>(false);
+  public readonly error = signal<string>('');
+  public readonly loggedIn = signal<boolean>(false);
 
   public readonly title: string = 'ng-app';
+
+  public handleLogin(): void {
+    this.loggedIn.set(!this.loggedIn());
+  }
 
   public loadRandomDogImage(): void {
     this.loading.set(true);
     this.error.set('');
 
     this.httpClient
-      .get<DogResponse>("https://dog.ceo/api/breeds/image/random")
+      .get<DogResponse>('https://dog.ceo/api/breeds/image/random')
       .subscribe({
         next: (response: DogResponse) => {
           if (response.status === 'success') {
@@ -46,7 +59,7 @@ export class AppComponent {
           this.data.set('');
           this.error.set('Error loading image: ' + err.message);
           this.loading.set(false);
-        }
+        },
       });
   }
 }
---
+++ frontend/ng-app/src/app/auth.directive.ts
@@ -0,0 +1,27 @@
+import {
+  Directive,
+  ElementRef,
+  inject,
+  input,
+  InputSignal,
+} from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+
+@Directive({
+  selector: '[authStatus]',
+})
+export class AuthDirective {
+  private readonly hostElement: ElementRef<HTMLElement> = inject(ElementRef);
+
+  public readonly authStatus: InputSignal<boolean> = input.required<boolean>();
+
+  constructor() {
+    toObservable(this.authStatus).subscribe((authStatus) => {
+      if (authStatus) {
+        this.hostElement.nativeElement.classList.remove('hidden');
+      } else {
+        this.hostElement.nativeElement.classList.add('hidden');
+      }
+    });
+  }
+}
