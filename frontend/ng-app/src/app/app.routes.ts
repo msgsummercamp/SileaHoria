3d2216f9b68f6cbbdc48bde5e5851b52b4d8c0e9
Silea-Horia <horia.silea@gmail.com>
2025-07-24 11:20:04 +0300

    added routes to app.routes.ts

--- frontend/ng-app/src/app/app.routes.ts
+++ frontend/ng-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: AppComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent },
+];
