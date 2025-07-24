23123368892f9f1605533a22ca162bd6f1983734
Silea-Horia <horia.silea@gmail.com>
2025-07-24 11:19:49 +0300

    removed unnecessary styleUrls

--- frontend/ng-app/src/app/not-found/not-found.component.ts
+++ frontend/ng-app/src/app/not-found/not-found.component.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-not-found',
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {}
