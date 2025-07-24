0de2686ae539971770224fd93d51e73079f30d5a
Silea-Horia <horia.silea@gmail.com>
2025-07-24 11:34:26 +0300

    modified navbar to use routerLink

--- frontend/ng-app/src/app/app.component.ts
+++ frontend/ng-app/src/app/app.component.ts
@@ -1,65 +1,22 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 import { NgOptimizedImage } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthDirective } from './auth.directive';
-
-type DogResponse = {
-  status: string;
-  message: string;
-};
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    MatButton,
-    MatToolbar,
-    NgOptimizedImage,
-    NotFoundComponent,
-    AuthDirective,
-  ],
+  imports: [MatButton, MatToolbar, NgOptimizedImage, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private readonly httpClient: HttpClient = inject(HttpClient);
-
-  public readonly data = signal<string>('');
-  public readonly loading = signal<boolean>(false);
-  public readonly error = signal<string>('');
   public readonly loggedIn = signal<boolean>(false);
 
   public readonly title: string = 'ng-app';
 
   public handleLogin(): void {
     this.loggedIn.set(!this.loggedIn());
-  }
-
-  public loadRandomDogImage(): void {
-    this.loading.set(true);
-    this.error.set('');
-
-    this.httpClient
-      .get<DogResponse>('https://dog.ceo/api/breeds/image/random')
-      .subscribe({
-        next: (response: DogResponse) => {
-          if (response.status === 'success') {
-            this.data.set(response.message);
-            this.error.set('');
-          } else {
-            this.data.set('');
-            this.error.set('Failed to load dog image.');
-          }
-          this.loading.set(false);
-        },
-        error: (err: Error) => {
-          this.data.set('');
-          this.error.set('Error loading image: ' + err.message);
-          this.loading.set(false);
-        },
-      });
   }
 }
--- frontend/ng-app/src/app/app.routes.ts
+++ frontend/ng-app/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'home', component: AppComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NotFoundComponent },
 ];
