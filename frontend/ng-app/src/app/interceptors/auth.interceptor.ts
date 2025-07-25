9a17e4e0d3ebc9a1c5b2076b6f0f4f8b3a9b9632
Silea-Horia <horia.silea@gmail.com>
2025-07-25 09:25:38 +0300

    resolved cors

--- frontend/ng-app/src/app/interceptors/auth.interceptor.ts
+++ frontend/ng-app/src/app/interceptors/auth.interceptor.ts
@@ -2,6 +2,13 @@ import { HttpEventType, HttpInterceptorFn } from '@angular/common/http';
 import { tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const skipUrls = ['https://dog.ceo/api/breeds/image/random'];
+  const shouldSkip = skipUrls.includes(req.url);
+
+  if (shouldSkip) {
+    return next(req);
+  }
+
   const cloned = req.clone({
     setHeaders: {
       Authorization: 'Bearer token',
