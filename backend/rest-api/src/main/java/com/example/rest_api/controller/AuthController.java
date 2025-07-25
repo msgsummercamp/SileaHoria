fc1f4a0183920a3e04b8c829167515560fb60d08
Silea-Horia <horia.silea@gmail.com>
2025-07-25 15:11:54 +0300

    fixed autowired

--- backend/rest-api/src/main/java/com/example/rest_api/controller/AuthController.java
+++ backend/rest-api/src/main/java/com/example/rest_api/controller/AuthController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
