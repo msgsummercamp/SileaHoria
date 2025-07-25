d53c8b2848f33e8c7c32eb9dddef083af683e0dc
Silea-Horia <horia.silea@gmail.com>
2025-07-25 10:28:33 +0300

    created form

--- frontend/ng-app/src/app/login/login.component.ts
+++ frontend/ng-app/src/app/login/login.component.ts
@@ -2,17 +2,49 @@ import { Component, inject, Signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { AuthService } from '../services/auth.service';
 import { AuthDirective } from '../auth.directive';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+type LogInForm = {
+  email: FormControl<string>;
+  password: FormControl<string>;
+};
 
 @Component({
   selector: 'app-login',
-  imports: [MatButton, AuthDirective],
+  imports: [MatButton, AuthDirective, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  private authService: AuthService = inject(AuthService);
+  private readonly authService = inject(AuthService);
+  private readonly formBuilder = inject(NonNullableFormBuilder);
+
+  protected readonly loginFormGroup: FormGroup =
+    this.formBuilder.group<LogInForm>({
+      email: this.formBuilder.control('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: this.formBuilder.control('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
 
   public readonly loggedIn: Signal<boolean> = this.authService.loggedIn;
+
+  public onFormSubmit() {
+    if (this.loginFormGroup.valid) {
+      this.handleLogin();
+      console.log(this.loginFormGroup.getRawValue());
+    }
+  }
 
   public handleLogin() {
     this.authService.login();
